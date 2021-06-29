dddea28e9a42c9a211c61e16c6b40944ad0f5d72
wcs <wenchangshou@live.cn>
2021-06-29 18:06:36 +0800

    update

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/wenchangshou2/vd-node-manage/bootstrap"
 	"github.com/wenchangshou2/vd-node-manage/pkg/conf"
+	"github.com/wenchangshou2/vd-node-manage/pkg/discovery"
 	"github.com/wenchangshou2/vd-node-manage/pkg/logging"
 	"github.com/wenchangshou2/vd-node-manage/routers"
 	"github.com/wenchangshou2/zutil"
@@ -20,6 +22,10 @@ func init() {
 	bootstrap.Init(confPath)
 }
 func main() {
+	if err := discovery.InitDiscovery("0.0.0.0", 8889); err != nil {
+		fmt.Println("初始化discovery失败")
+		return
+	}
 	api := routers.InitRouter()
 	if err := api.Run(conf.SystemConfig.Listen); err != nil {
 		logging.G_Logger.Warn("无法监听[" + conf.SystemConfig.Listen + "]" + "," + err.Error())
